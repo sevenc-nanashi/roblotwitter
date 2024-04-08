88037fea8e8f343cb826a7aea2cb82c7788b123a
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-04-08 16:15:58 +0900

    Add: Add my profile

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,22 @@ onMounted(() => {
   <div>
     <h1>{{ t("title") }}</h1>
     <p class="main">{{ t("description") }}</p>
-    <a href="https://github.com/sevenc-nanashi/roblotwitter">{{ t("link") }}</a>
+    <a href="https://github.com/sevenc-nanashi/roblotwitter">{{ t("link") }}</a><br />
+    <a href="https://sevenc7c.com" class="author">{{ t("author") }}</a>
   </div>
 </template>
 
 <style scoped>
 .main {
   white-space: pre-line;
+}
+.author {
+  display: block;
+  padding-top: 1rem;
+  color: #48b0d5;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
 }
 </style>
